2ed821c33a94077d3249e176678ff3f6594acac3
Reisi <mohamad.mahdir2002@gmail.com>
2022-01-04 19:38:40 +0330

    add add_date to project

--- database_appender.py
+++ database_appender.py
@@ -17,8 +17,20 @@ def add_person(person: variable.Person):
     data.commit()
     data.close()
 
+def add_date(date: variable.Date):
+    data = sqlite3.connect('data.db')
+    cur = data.cursor()
+    cur.execute(f"""
+        INSERT INTO dates(year, month, day, weekday)
+        VALUES("{date.year}", {date.month},{date.day}, "{date.week_day}");
+    """)
+
+    data.commit()
+    data.close()
 
 if __name__ == '__main__':
     setup()
     someone = variable.Person("Mohammad Mahdi Reisi", 19, 'male')
     add_person(someone)
+    somedate = variable.Date(2022, 11, 21)
+    add_date(somedate)
--- database_maker.py
+++ database_maker.py
@@ -2,65 +2,62 @@
 import sqlite3
 
 def run():
-	try:
-		data = sqlite3.connect('data.db')
-		cur = data.cursor()
 
-		cur.execute("""
-		CREATE TABLE IF NOT EXISTS places (
-			id integer PRIMARY KEY,
-			name text NOT NULL,
-			address text NOT NULL,
-			distance integer
-		);
-		""")
+	data = sqlite3.connect('data.db')
+	cur = data.cursor()
 
-		cur.execute("""
-		CREATE TABLE IF NOT EXISTS persons (
-			id integer PRIMARY KEY,
-			name text NOT NULL,
-			age integer,
-			gender text
-		);
-		""")
+	cur.execute("""
+	CREATE TABLE IF NOT EXISTS places (
+		id integer PRIMARY KEY,
+		name text NOT NULL,
+		address text NOT NULL,
+		distance integer
+	);
+	""")
 
-		cur.execute("""
-		CREATE TABLE IF NOT EXISTS dates (
-			id integer PRIMARY KEY,
-			year integer,
-			month integer,
-			day integer,
-			weekday text NOT NULL
-		);
-		""")
+	cur.execute("""
+	CREATE TABLE IF NOT EXISTS persons (
+		id integer PRIMARY KEY,
+		name text NOT NULL,
+		age integer,
+		gender text
+	);
+	""")
 
-		cur.execute("""
-		CREATE TABLE IF NOT EXISTS payment_methods (
-			id integer PRIMARY KEY,
-			name text NOT NULL,
-			number text NOT NULL
-		);
-		""")
+	cur.execute("""
+	CREATE TABLE IF NOT EXISTS dates (
+		id integer PRIMARY KEY,
+		year integer,
+		month integer,
+		day integer,
+		weekday text NOT NULL
+	);
+	""")
 
-		cur.execute("""
-		CREATE TABLE IF NOT EXISTS expenses (
-			id integer PRIMARY KEY,
-			amount integer,
-			explanation text,
-			discount integer,
-			place integer NOT NULL,
-			date integer NOT NULL,
-			person integer NOT NULL,
-			payment_method integer NOT NULL,
-			FOREIGN KEY (place) REFERENCES places(id),
-			FOREIGN KEY (date) REFERENCES dates(id),
-			FOREIGN KEY (person) REFERENCES persons(id),
-			FOREIGN KEY (payment_method) REFERENCES payment_methods(id) 
-		);
-		""")
+	cur.execute("""
+	CREATE TABLE IF NOT EXISTS payment_methods (
+		id integer PRIMARY KEY,
+		name text NOT NULL,
+		number text NOT NULL
+	);
+	""")
 
-		data.commit()
-		data.close()
-		return 1
-	except:
-		return 0
+	cur.execute("""
+	CREATE TABLE IF NOT EXISTS expenses (
+		id integer PRIMARY KEY,
+		amount integer,
+		explanation text,
+		discount integer,
+		place integer NOT NULL,
+		date integer NOT NULL,
+		person integer NOT NULL,
+		payment_method integer NOT NULL,
+		FOREIGN KEY (place) REFERENCES places(id),
+		FOREIGN KEY (date) REFERENCES dates(id),
+		FOREIGN KEY (person) REFERENCES persons(id),
+		FOREIGN KEY (payment_method) REFERENCES payment_methods(id) 
+	);
+	""")
+
+	data.commit()
+	data.close()
