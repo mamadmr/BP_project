9d0f4ea1f52b12cdebedd63aa26372680733c46f
Reisi <mohamad.mahdir2002@gmail.com>
2022-01-12 22:30:11 +0330

    finish add_to_database functions

--- variable.py
+++ variable.py
@@ -9,21 +9,25 @@
 
 import datetime
 import sqlite3
-
+import database_maker
 
 class Place:
     def __init__(self, name: str, distance: int, address: str) -> None:
         """
             the place could be a website or a shop or an organization 
             if the place is website put 0 as distance
+            if the variable added is equal to one then means this place added to data base 
         """
         self.name = name
         self.distance = distance
         self.address = address
         self.added = 0
+        self.id = -1
 
     def add_to_database(self)-> None:
-        """just to add the object to database"""
+        """just to add the object to database if it didn't add to database"""
+        if self.added:
+            return
         self.added = 1
         data = sqlite3.connect('data.db')
         cur = data.cursor()
@@ -32,18 +36,22 @@ class Place:
         VALUES("{self.name}", "{self.address}",{self.distance});
         """)
 
+        self.id = cur.lastrowid
         data.commit()
         data.close()
 
 class Date:
     def __init__(self, year: int, month: int, day: int) -> None:
+        """objects are dates respect to year month and day """
         self.year = year
         self.month = month
         self.day = day
         self.added = 0
         self._compute_week_day()
+        self.id = -1
     
     def _compute_week_day(self) -> None:
+        """to compute week day of the date """
         intDay = datetime.date(year=self.year, month=self.month, day=self.day).weekday()
         days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         self.week_day = days[intDay]
@@ -55,6 +63,8 @@ class Date:
 
     def add_to_database(self)-> None:
         """just to add the object to database"""
+        if self.added:
+            return
         self.added = 1
         data = sqlite3.connect('data.db')
         cur = data.cursor()
@@ -63,18 +73,23 @@ class Date:
             VALUES({self.year}, {self.month},{self.day}, "{self.week_day}");
         """)
 
+        self.id = cur.lastrowid
         data.commit()
         data.close()
 
 class Person:
     def __init__(self, name: str, age: int, gender: str) -> None:
+        """each object is a person who can buy things"""
         self.name = name
         self.age = age
         self.gender = gender
         self.added = 0
+        self.id = -1
 
     def add_to_database(self)-> None:
         """just to add the object to database"""
+        if self.added:
+            return
         self.added = 1
         data = sqlite3.connect('data.db')
         cur = data.cursor()
@@ -82,7 +97,7 @@ class Person:
             INSERT INTO persons(name, age, gender)
             VALUES("{self.name}", {self.age}, "{self.gender}");
         """)
-
+        self.id = cur.lastrowid
         data.commit()
         data.close()
 
@@ -92,6 +107,7 @@ class PaymentMethod:
         self.name = name
         self.number = number
         self.added = 0
+        self.id = -1
 
     def add_to_database(self)-> None:
         """just to add the object to database"""
@@ -103,6 +119,7 @@ class PaymentMethod:
             VALUES("{self.name}", "{self.number}");
         """)
 
+        self.id = cur.lastrowid
         data.commit()
         data.close()
 
@@ -117,22 +134,45 @@ class Expense:
         self.payment_method = payment_method
         self.discount = discount
         self.added = 0
+        self.id = -1
 
     def add_to_database(self)-> None:
         """just to add the object to database"""
+        if self.added:
+            return
         self.added = 1
-        
+        data = sqlite3.connect('data.db')
+        cur = data.cursor()
+        cur.execute(f"""
+            INSERT INTO expenses(amount, explanation, discount, place, date, person, payment_method)
+            VALUES("{self.amount}", "{self.explanation}", {self.discount}
+            , {self.place.id}, {self.date.id}, {self.person.id}, {self.payment_method.id});
+        """)
+        self.id = cur.lastrowid
+        data.commit()
+        data.close()
 
 if __name__ == "__main__":
+    database_maker.run()
+
     date = Date(2022, 1, 2)
-    print(*date.show(), sep=' - ')
+    date.add_to_database()
+#    print(*date.show(), sep=' - ')
+
     amount = 1000
-    place = Place("Amazon", 0, 'www.amazon.de')
     explanation = 'buy some pens and notebooks'
-    person = Person("Mohammad Mahdi Reisi", 13, 'male')
-    payment_method = PaymentMethod("visa card", '1234-5678-1234-1234')
     discount = 0
-    expense = Expense(amount, place, date, explanation, person, payment_method, discount)
-    print(expense.date.week_day)
+
+    place = Place("Amazon", 0, 'www.amazon.de')
+    place.add_to_database()
+
+    person = Person("Mohammad Mahdi Reisi", 13, 'male')
     person.add_to_database()
+
+    payment_method = PaymentMethod("visa card", '1234-5678-1234-1234')
     payment_method.add_to_database()
+
+    expense = Expense(amount, place, date, explanation, person, payment_method, discount)
+    expense.add_to_database()
+
+    print(expense.date.week_day)
