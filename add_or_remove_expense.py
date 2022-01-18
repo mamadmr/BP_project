697566c6b8c159be91137763e1ac9f13feba3846
Reisi <mohamad.mahdir2002@gmail.com>
2022-01-18 14:25:56 +0330

    add button function is finish

--- add_or_remove_expense.py
+++ add_or_remove_expense.py
@@ -1,19 +1,167 @@
 import tkinter as tk
+from numpy import var
 from tkcalendar import DateEntry
 import add_or_remove_payment_method
 import add_or_remove_place
 import add_or_remove_person_display
+from tkinter import messagebox
+import search_date
+import search_persons
+import search_place
+import search_payment_methods
+import variable
 
 class AddRemoveExpense:
     def __init__(self) -> None:
         self.win = tk.Tk()
         self.win.title('add or remove expense')
         self.win.geometry('400x500')
+    
+    def read_text(self):
+        inp = []
+        inp.append(self.amount.get("1.0",'end-1c'))
+        inp.append(self.place.get("1.0",'end-1c'))
+        inp.append(self.date.get())
+        inp.append(self.explantion.get("1.0",'end-1c'))
+        inp.append(self.person.get("1.0",'end-1c'))
+        inp.append(self.payment_method.get("1.0",'end-1c'))
+        inp.append(self.discount.get("1.0",'end-1c'))
+        inp.append(self.amount_from.get("1.0",'end-1c'))
+        inp.append(self.amount_to.get("1.0",'end-1c'))
+        inp.append(self.date_from.get())
+        inp.append(self.date_to.get())
+        inp.append(self.discount_from.get("1.0",'end-1c'))
+        inp.append(self.discount_to.get("1.0",'end-1c'))
+        return inp
+
+    def translate_time(self, date):
+        year, month, day = date.split('/')
+        year, month, day = int(year), int(month), int(day)
+        date = variable.Date(year, month, day)
+        sch = search_date.SearchDate()
+        sch.by_date(date, date)
+        temp = sch.run()
+        if len(temp) == 0:
+            date.add_to_database()
+            return date
+        return temp[0]
+
+    def data_filter(self) -> list:
+        """check if data are correct  and return which text are filled """
+        data = self.read_text()
+        flag = [0 for i in range(13)]
+        for i in range(len(data)):
+            if data[i] != '':
+                flag[i] = 1
+        
+        if flag[0] and not data[0].isdigit():
+            messagebox.showerror('amount Error', 'amount is not intiger')
+            return []
+        
+        if flag[1]:
+            sch = search_place.SearchPlace()
+            sch.by_name(data[1])
+            out = sch.run()
+            if len(out) == 0:
+                messagebox.showerror('place Error', 'there is not such a place')
+                return []
+            else:
+                data[1] = out[0]
+        
+        if flag[2]:
+            try:
+                data[2] = self.translate_time(data[2])
+            except:
+                messagebox.showerror('date Error', 'the date is not correct')
+                return []
+        
+        if flag[4]:
+            sch = search_persons.SearchPerson()
+            sch.by_name(data[4])
+            out = sch.run()
+            if len(out) == 0:
+                messagebox.showerror('person Error', 'there is not such a person')
+                return []
+            else:
+                data[4] = out[0]
+
+        if flag[5]:
+            sch = search_payment_methods.SearchPaymentMethod()
+            sch.by_name(data[5])
+            out = sch.run()
+            if len(out) == 0:
+                messagebox.showerror('payment method Error', 'there is not such a payment method')
+                return []
+            else:
+                data[5] = out[0]
+        
+        if flag[6] and not data[6].isdigit():
+            messagebox.showerror('discount Error', 'discount is not intiger')
+            return []
+        
+        if flag[7] and not data[7].isdigit():
+            messagebox.showerror('amount Error', 'amount is not intiger')
+            return []
+
+        if flag[8] and not data[8].isdigit():
+            messagebox.showerror('amount Error', 'amount is not intiger')
+            return []
+
+        if flag[9]:
+            try:
+                data[9] = self.translate_time(data[9])
+            except:
+                messagebox.showerror('date Error', 'the date is not correct')
+                return []
+        
+        if flag[10]:
+            try:
+                data[10] = self.translate_time(data[10])
+            except:
+                messagebox.showerror('date Error', 'the date is not correct')
+                return []
+        
+        if flag[11] and not data[11].isdigit():
+            messagebox.showerror('discount Error', 'discount is not intiger')
+            return []
+
+        if flag[12] and not data[12].isdigit():
+            messagebox.showerror('discount Error', 'discount is not intiger')
+            return []
+
+        return data, flag
+    
+    def reset(self):
+        self.amount.delete("1.0",'end-1c')
+        self.place.delete("1.0",'end-1c')
+        self.date._set_text(self.date._date.strftime(''))
+        self.explantion.delete("1.0",'end-1c')
+        self.person.delete("1.0",'end-1c')
+        self.payment_method.delete("1.0",'end-1c')
+        self.discount.delete("1.0",'end-1c')
+        self.amount_to.delete("1.0",'end-1c')
+        self.amount_from.delete("1.0",'end-1c')
+        self.date_to._set_text(self.date_to._date.strftime(''))
+        self.date_from._set_text(self.date_from._date.strftime(''))
+        self.discount_from.delete("1.0",'end-1c')
+        self.discount_to.delete("1.0",'end-1c')
 
     def add_button(self)->None:
         """this method runs when you push the add button"""
-        pass
+        data, flag = self.data_filter()
+        if len(flag) == 0:
+            self.reset()
+            return
 
+        if flag[0]+flag[1]+flag[2] + flag[3]+ flag[4]+ flag[5]+ flag[6] != 7:
+            messagebox.showerror('empty input', 'please fill Entries')
+            self.reset()
+            return
+
+        expense = variable.Expense(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
+        expense.add_to_database()
+        self.reset()
+        
     def remove_button(self) -> None:
         """this method runs when client push remove button"""
         pass
@@ -81,7 +229,9 @@ class AddRemoveExpense:
         self.place = tk.Text(self.win,height=1,width=12)
         self.place.place(relx=x, rely=y+down, anchor = 'w')
 
-        self.date=DateEntry(self.win,selectmode='day',height=1,width=30)
+        self.date=DateEntry(self.win,selectmode='day',height=1,width=30, date_pattern='yyyy/mm/dd')
+        self.date.configure(validate='none')
+        self.date._set_text(self.date._date.strftime(''))
         self.date.place(relx=x, rely=y+down*2, anchor = 'w')
 
         self.explantion = tk.Text(self.win,height=1,width=25)
@@ -102,11 +252,18 @@ class AddRemoveExpense:
         self.amount_to = tk.Text(self.win,height=1,width=8)
         self.amount_to.place(relx=x+0.335, rely=y+down*7, anchor = 'w')
 
-        self.date_from=DateEntry(self.win,selectmode='day',height=1,width=8)
+        self.date_from=DateEntry(self.win,selectmode='day',height=1,width=10, date_pattern='yyyy/mm/dd')
+        self.date_from.configure(validate='none')
+        self.date_from._set_text(self.date_from._date.strftime(''))
         self.date_from.place(relx=x, rely=y+down*8, anchor = 'w')
+
         tk.Label(self.win, text='to: ').place(relx=x+0.25, rely=y+down*8, anchor = 'center')
-        self.date_to=DateEntry(self.win,selectmode='day',height=1,width=8)
+        
+        self.date_to=DateEntry(self.win,selectmode='day',height=1,width=10, date_pattern='yyyy/mm/dd')
+        self.date_to.configure(validate='none')
+        self.date_to._set_text(self.date_to._date.strftime(''))
         self.date_to.place(relx=x+0.335, rely=y+down*8, anchor = 'w')
+
 
         self.discount_from = tk.Text(self.win,height=1,width=8)
         self.discount_from.place(relx=x, rely=y+down*9, anchor = 'w')
