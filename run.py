0bb71544b2d2aa14e8e18479a7cdb5dbbcd16717
Reisi <mohamad.mahdir2002@gmail.com>
2022-01-19 15:57:54 +0330

    bug fixed

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 """check if library exists if not install them """
 
+import os
 
 try:
     import tkcalendar
     import matplotlib.pyplot as plt
     import database_maker
     import run_main_menu
-    import os
 except:
     os.system("pip install tkcalendar")
     os.system("pip install matplotlib")
 
 import database_maker
 import run_main_menu
-import os
+
 database_maker.run()
 run_main_menu.Menu().run()
