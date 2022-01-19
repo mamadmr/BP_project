f9c632c0d6ef4d2925f09136ee84e0b94541fb19
Reisi <mohamad.mahdir2002@gmail.com>
2022-01-19 18:26:36 +0330

    bug fiexed

--- run.py
+++ run.py
@@ -5,14 +5,12 @@ import os
 try:
     import tkcalendar
     import matplotlib.pyplot as plt
-    import database_maker
-    import run_main_menu
 except:
     os.system("pip install tkcalendar")
     os.system("pip install matplotlib")
 
 import database_maker
 import run_main_menu
-
+  
 database_maker.run()
 run_main_menu.Menu().run()
